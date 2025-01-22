cda7992b7ba7041e61d6c6a429a96dbe9ecb09fe
BohdannaOhirko <bohdannaohirko@gmail.com>
2025-01-22 23:50:52 +0200

    event has not replaced

--- trelloTasks/contextAnd_this_call_apply_bind/this_call_apply_bind.js
+++ trelloTasks/contextAnd_this_call_apply_bind/this_call_apply_bind.js
@@ -8,29 +8,41 @@
 
 // Використовуй bind, call, або apply для прив'язування контексту.
 
-const bookingRoom = [
-  { room: 1, price: 100 },
-  { room: 2, price: 200 },
-];
-
-for (let i = 0; i < bookingRoom.length; i++) {
-  (function (i) {
-    this.displayInfo = function () {
-      console.log(`Booked room №${this.room} at price ${this.price}$`);
+const user = {
+  name: "Alice",
+  bookings: [],
+  bookRoom(roomNumber, price, checkIn, checkOut) {
+    const booking = {
+      roomNumber,
+      price,
+      checkIn,
+      checkOut,
     };
-
-    this.displayInfo();
-  }).call(bookingRoom[i], i);
-}
-const roomType = ["standard", "junior-suite", "suite"];
-const bookingHotelRoom = {
-  arrivalDate: new Date(),
-  departureDate: new Date(),
-  createBooking() {
-    console.log(`You have booked '${this.roomType[2]}`);
+    this.bookings.push(booking);
+    console.log(`Room ${roomNumber} booked for ${this.name} at ${price}$`);
+    return this;
   },
 };
 
-// bookingHotelRoom.apply(this, [roomType]);
-// https://ru.stackoverflow.com/questions/1472449/%D0%9A%D0%B0%D0%BA-%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D1%8C-%D0%B1%D1%80%D0%BE%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F-%D0%BD%D0%B0-js-html-css
-//https://www.freecodecamp.org/ukrainian/news/posibnyk-z-masyvu-ob-yektiv-javascript-yak-stvoryuvaty-onovlyuvaty-ta-perehlyadaty-ob-yekty-za-dopomohoyu-metodiv-masyvu-js/
+const user2 = {
+  name: "Bob",
+  bookings: [],
+};
+const user3 = {
+  name: "Nina",
+  bookings: [],
+};
+// console.log(user.bookRoom(101, 200, "2025-01-01", "2025-01-02"));
+
+user.bookRoom.call(user2, 101, 200, "2025-01-15", "2025-01-20");
+
+const bookForBob = user.bookRoom.bind(user3);
+bookForBob(202, 300, "2025-02-01", "2025-02-05");
+
+console.log(user.bookings);
+console.log(user2.bookings);
+console.log(user3.bookings);
+
+// У user є метод bookRoom, який створює об'єкт бронювання і додає його в масив booking
+// потім ми викликаємо bookRoom для іншого об'єкта user2 і за допомогою call одразу передаємо аргументи
+// а за допомогою bind створюємо функцію, яка завжди працює з контекстом user3
