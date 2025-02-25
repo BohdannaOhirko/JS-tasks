53916d918bf25945b68a443a033648a084c6ab51
BohdannaOhirko <bohdannaohirko@gmail.com>
2025-02-25 16:29:37 +0200

    does not useItems

--- JS_tasks/interactiveGame/script.js
+++ JS_tasks/interactiveGame/script.js
@@ -4,6 +4,11 @@
 // Монстр (Monster) – об'єкт з властивостями: вид, здоров'я, сила.
 
 // Предмет (Item) – об'єкт з властивостями: назва, тип (зброя, ліки), ефект.
+const btnAttack = document.querySelector(".btn-attack");
+const messageMonster = document.getElementById("message-monster");
+const messagePlayer = document.getElementById("message-player");
+const playerHealth = document.getElementById("player-health");
+const monsterHealth = document.getElementById("monster-health");
 
 class Character {
   constructor(name, health, strength, medicine) {
@@ -11,25 +16,77 @@ class Character {
     this.health = health;
     this.strength = strength;
     this.medicine = medicine;
+    this.items = [];
   }
   attack() {
     let damage = Math.floor(Math.random() * this.strength) + 1; // Випадковий урон
     monster.health -= damage; // Зменшуємо здоров'я
-    console.log(
-      `${this.name} атакує ${monster.name}, завдаючи ${damage} шкоди.`
-    );
+    let playerAttack = `${this.name} атакує ${monster.name}, завдаючи ${damage} шкоди.`;
+    let messageAttack;
     if (monster.health <= 0) {
       monster.health = 0;
-      console.log(`${monster.name} переможений`);
+      monsterHealth.innerHTML = 0;
+      messageAttack = `${monster.name} переможений`;
     } else {
-      console.log(`${monster.name} має ${monster.medicine} ліків`);
+      messageAttack = `${monster.name} має ${monster.medicine} ліків`;
+    }
+    return { first: playerAttack, second: messageAttack };
+  }
+  useItems(items, index) {
+    if (items[index]) {
+      const item = item[index];
+      console.log(`${this.name} використовує ${item.name}`);
+
+      if (this.items.includes(item)) {
+        item.activate(this, monster);
+        this.items = this.items.filter((i) => i !== item);
+      } else {
+        console.log("Предмет не знайдено!");
+      }
     }
   }
 }
-
 const player = new Character("player-1", 100, 20, 10);
 const monster = new Character("monster", 100, 15, 10);
-player.attack(player);
+player.useItems(item, 0);
+btnAttack.addEventListener("click", (e) => {
+  e.preventDefault();
+  const values = player.attack();
+  messagePlayer.innerHTML = values.first;
+  messageMonster.innerHTML = values.second;
+  player.useItems(potion);
+});
+
+class Item {
+  constructor(name, type, effect) {
+    this.name = name;
+    this.type = type;
+    this.effect = effect;
+  }
+  activate(player, monster) {
+    if (this.type === "weapon") {
+      let extraDamage = this.effect;
+      monster.health -= extraDamage;
+      console.log(
+        `${player.name} використовує ${this.name} і збільшує атакувальну силу на  ${this.effect} %!`
+      );
+    } else if (this.type === "shield") {
+      player.health += this.effect;
+      console.log(
+        `${player.name} використовує ${this.name} і збільшує захист на ${this.effect} %`
+      );
+    } else if (this.type === "magic-item") {
+      player.energy += this.effect;
+      console.log(
+        `${player.name} використовує ${this.name} і відновлює ${this.effect} здоров'я!`
+      );
+    }
+  }
+}
+const sword = new Item("Меч світла", "weapon", 20);
+const shield = new Item("Щит древніх", "shield", 30);
+const potion = new Item("Зілля відновлення", "magic-item", 50);
+const items = [sword, shield, potion];
 
 class Monster {
   constructor(type, health, strength) {
@@ -38,12 +95,4 @@ class Monster {
     this.strength = strength;
   }
 }
-const monsterTruck = new Monster("monster-1", 100, 90);
-
-class Item {
-  constructor(name, type, effect) {
-    this.name = name;
-    this.type = type;
-    this.effect = effect;
-  }
-}
+const monster1 = new Monster("monster-1", 100, 90);
