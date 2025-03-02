2283d9430a4a88730e8b82ddc8054af46e0bb664
BohdannaOhirko <bohdannaohirko@gmail.com>
2025-03-02 13:09:57 +0200

    new tasks

--- codewars/script.js
+++ codewars/script.js
@@ -38,3 +38,73 @@ function addBinary(a, b) {
   return binary;
 }
 console.log(addBinary(7, 4));
+
+// Implement a function that computes the difference between two lists.
+// The function should remove all occurrences of elements from the first list (a)
+// that are present in the second list (b). The order of elements in the first list should be preserved
+// in the result.
+
+// Examples
+// If a = [1, 2] and b = [1], the result should be [2].
+
+// If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+
+function arrayDiff(arr1, arr2) {
+  return arr1.filter((item) => !arr2.includes(item));
+}
+console.log(arrayDiff([1, 2], [1]));
+
+// Complete the function that accepts a string parameter, and reverses each word in the string.
+//  All spaces in the string should be retained.
+
+function reverseWords(str) {
+  let reverseStr = str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+  return reverseStr;
+}
+console.log(reverseWords("This is an example!"));
+
+// Check to see if a string has the same amount of 'x's and 'o's.
+// The method must return a boolean and be case insensitive.
+// The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+  let countX = str.toLowerCase().split("x").length - 1;
+  let countY = str.toLowerCase().split("o").length - 1;
+  console.log(countY);
+  if (countX === countY) {
+    return true;
+  }
+  if (!countX || !countY) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(XO("zzzzO"));
+
+// You are going to be given a non-empty string.
+// Your job is to return the middle character(s) of the string.
+
+// If the string's length is odd, return the middle character.
+// If the string's length is even, return the middle 2 characters.
+// Examples:
+// "test" --> "es"
+// "testing" --> "t"
+// "middle" --> "dd"
+// "A" --> "A"
+function getMiddle(s) {
+  //Code goes here!
+  return "";
+}
