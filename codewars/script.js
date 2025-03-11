db1528197790f1d70c8bfe7d4008414509dda6a2
BohdannaOhirko <bohdannaohirko@gmail.com>
2025-03-11 16:29:24 +0200

    more tasks

--- codewars/script.js
+++ codewars/script.js
@@ -198,3 +198,25 @@ function maskify(cc) {
     .replaceAll(",", "");
 }
 console.log(maskify("4556364607935616"));
+
+// Your task is to make a function that can take any non-negative integer as an argument
+//  and return it with its digits in descending order. Essentially, rearrange the digits
+//  to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+  if (n > 0) {
+    let arr = String(n).split("").map(Number);
+    let descending = arr.sort((a, b) => b - a).join("");
+    console.log(typeof parseInt(descending));
+    return parseInt(descending);
+  }
+}
+
+console.log(descendingOrder(42145));
